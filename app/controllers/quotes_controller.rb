57021e8a849bcf472b350522901d31b2efc9c6e7
Justin Hooper <justin_hooper@yahoo.com>
2019-06-03 19:46:57 +0000

    Lesson 15 building the about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,6 @@
 class QuotesController < ApplicationController
-  
   def index
-
-      @quote = Quote.order("RANDOM()").first
+    @quote = Quote.order("RANDOM()").first
   end
 
   def new
@@ -11,15 +9,16 @@ class QuotesController < ApplicationController
 
   def create
     @quote = Quote.create(quote_params)
-  if @quote.invalid?
-    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-  end
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
     redirect_to root_path
   end
 
+  def about
+  end
 
   private
-
 
   def quote_params
     params.require(:quote).permit(:saying, :author)
